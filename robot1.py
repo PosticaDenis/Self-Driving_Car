e37ac63ad2c3e66a8fbe06b2260d0655c96f1e30
PosticaDenis <postica.denis.95@gmail.com>
2018-05-21 00:17:54 +0300

    update

--- robot1.py
+++ robot1.py
@@ -6,8 +6,8 @@ import Tkinter as tk
 FRIGHT_1 = 23
 FRIGHT_2 = 24
 
-BRIGHT_2 = 17
 BRIGHT_1 = 27
+BRIGHT_2 = 17
 
 FLEFT_1 = 5
 FLEFT_2 = 6
@@ -26,7 +26,7 @@ def init():
     gpio.setup(BLEFT_1, gpio.OUT)
     gpio.setup(BLEFT_2, gpio.OUT)
 
-def forward(tf):
+def reverse(tf):
     init()
     gpio.output(FRIGHT_1, False)
     gpio.output(FRIGHT_2, True)
@@ -41,64 +41,14 @@ def forward(tf):
     time.sleep(tf)
     gpio.cleanup()
 
-
-def one(tf):
+def forward(tf):
     init()
     gpio.output(FRIGHT_1, True)
-    gpio.output(FRIGHT_2, False) #move forward
-
-    gpio.output(BRIGHT_1, False)
-    gpio.output(BRIGHT_2, False)
-
-    gpio.output(FLEFT_1, False)
-    gpio.output(FLEFT_2, False)
-    gpio.output(BLEFT_1, False)
-    gpio.output(BLEFT_2, False)
-
-    time.sleep(tf)
-    gpio.cleanup()
-
-def two(tf):
-    init()
-    gpio.output(FRIGHT_1, False)
     gpio.output(FRIGHT_2, False)
-
     gpio.output(BRIGHT_1, True)
     gpio.output(BRIGHT_2, False)
 
-    gpio.output(FLEFT_1, False)
-    gpio.output(FLEFT_2, False)
-    gpio.output(BLEFT_1, False)
-    gpio.output(BLEFT_2, False)
-
-    time.sleep(tf)
-    gpio.cleanup()
-
-def three(tf):
-    init()
-    gpio.output(FRIGHT_1, False)
-    gpio.output(FRIGHT_2, False)  # move back
-
-    gpio.output(BRIGHT_1, False)
-    gpio.output(BRIGHT_2, False)
-
     gpio.output(FLEFT_1, True)
-    gpio.output(FLEFT_2, False)
-    gpio.output(BLEFT_1, False)
-    gpio.output(BLEFT_2, False)
-
-    time.sleep(tf)
-    gpio.cleanup()
-
-def four(tf):
-    init()
-    gpio.output(FRIGHT_1, False)
-    gpio.output(FRIGHT_2, False)  # move back
-
-    gpio.output(BRIGHT_1, False)
-    gpio.output(BRIGHT_2, False)
-
-    gpio.output(FLEFT_1, False)
     gpio.output(FLEFT_2, False)
     gpio.output(BLEFT_1, True)
     gpio.output(BLEFT_2, False)
@@ -106,14 +56,12 @@ def four(tf):
     time.sleep(tf)
     gpio.cleanup()
 
-
-
-def reverse(tf):
+def fullright(tf):
     init()
-    gpio.output(FRIGHT_1, True)
-    gpio.output(FRIGHT_2, False)
-    gpio.output(BRIGHT_1, True)
-    gpio.output(BRIGHT_2, False)
+    gpio.output(FRIGHT_1, False)
+    gpio.output(FRIGHT_2, True)
+    gpio.output(BRIGHT_1, False)
+    gpio.output(BRIGHT_2, True)
 
     gpio.output(FLEFT_1, True)
     gpio.output(FLEFT_2, False)
@@ -127,42 +75,46 @@ def right(tf):
     init()
     gpio.output(FRIGHT_1, True)
     gpio.output(FRIGHT_2, True)
-    gpio.output(BRIGHT_1, False)
+    gpio.output(BRIGHT_1, True)
     gpio.output(BRIGHT_2, True)
+
+    gpio.output(FLEFT_1, True)
+    gpio.output(FLEFT_2, False)
+    gpio.output(BLEFT_1, True)
+    gpio.output(BLEFT_2, False)
 
     time.sleep(tf)
     gpio.cleanup()
 
-def reverseRight(tf):
+def fullleft(tf):
     init()
     gpio.output(FRIGHT_1, True)
-    gpio.output(FRIGHT_2, True)
+    gpio.output(FRIGHT_2, False)
     gpio.output(BRIGHT_1, True)
     gpio.output(BRIGHT_2, False)
+
+    gpio.output(FLEFT_1, False)
+    gpio.output(FLEFT_2, True)
+    gpio.output(BLEFT_1, False)
+    gpio.output(BLEFT_2, True)
 
     time.sleep(tf)
     gpio.cleanup()
 
 def left(tf):
     init()
-    gpio.output(FRIGHT_1, False)
-    gpio.output(FRIGHT_2, True)
-    gpio.output(BRIGHT_1, True)
-    gpio.output(BRIGHT_2, True)
-
-    time.sleep(tf)
-    gpio.cleanup()
-
-def reverseLeft(tf):
-    init()
     gpio.output(FRIGHT_1, True)
     gpio.output(FRIGHT_2, False)
     gpio.output(BRIGHT_1, True)
-    gpio.output(BRIGHT_2, True)
+    gpio.output(BRIGHT_2, False)
+
+    gpio.output(FLEFT_1, True)
+    gpio.output(FLEFT_2, True)
+    gpio.output(BLEFT_1, True)
+    gpio.output(BLEFT_2, True)
 
     time.sleep(tf)
     gpio.cleanup()
-
 
 def key_input(event):
     print "Key: ", event.char
@@ -174,21 +126,13 @@ def key_input(event):
     elif key_press.lower() == 's':
         reverse(st)
     elif key_press.lower() == 'a':
-        left(st)
+        fullleft(st)
     elif key_press.lower() == 'd':
+        fullright(st)
+    elif key_press.lower() == 'q':
+        left(st)
+    elif key_press.lower() == 'e':
         right(st)
-    elif key_press.lower() == 'z':
-        reverseLeft(st)
-    elif key_press.lower() == 'x':
-        reverseRight(st)
-    elif key_press.lower() == '1':
-        one(st)
-    elif key_press.lower() == '2':
-        two(st)
-    elif key_press.lower() == '3':
-        three(st)
-    elif key_press.lower() == '4':
-        four(st)
     else:
         print("Hell no to the no no ...")
 
